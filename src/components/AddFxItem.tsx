ad44213e9107e72730c6009bfe80a98ebb9f6e02
Christoffer Gylin <christoffer@christoffergylin.com>
2025-04-20 15:15:07 +0200

    Updated design for AddFxModal and AddFxItem

--- src/components/AddFxItem.tsx
+++ src/components/AddFxItem.tsx
@@ -4,7 +4,7 @@ import { type StateFx, type ToneFx, type AddFxType } from "../types/Fx"
 const AddFxItem = ({ fx, callback }: { fx: AddFxType, callback: (toneFx: ToneFx, stateFx: StateFx) => void }) => {
     return (
         <button
-            className="w-full py-2 hover:bg-slate-400/20 border-b border-slate-300/50 last:border-b-0"
+            className="flex flex-col gap-4 w-full py-4 px-8 hover:bg-slate-400/20 border-b border-slate-300/50 last:border-b-0 text-start cursor-pointer"
             onClick={() => {callback(fx.createToneFx(), fx.createStateFx())}}>
             <h2 className="text-2xl capitalize">{fx.name}</h2>
             <p className="text-lg">{fx.description}</p>
